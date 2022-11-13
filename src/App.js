1c1df137991d0ff92f79fdfb9934dce8675bc714
Sunny Prajapati <prajapatisunny222@gmail.com>
2022-11-13 17:18:08 +0530

    react component part-2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import { Navbar, NavbarBrand  } from 'reactstrap';
 import {Menu} from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 function App() {
+  const [dishes]=useState(DISHES);
   return (
     <div>
       <Navbar dark color='primary'>
         <div className='container'>
           <NavbarBrand href='/'>risetorante confusion</NavbarBrand>
         </div>
-      </Navbar>
-      <Menu/>
+      </Navbar >
+      <Menu dishes={dishes}/>
     </div>
   );
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,67 +1,61 @@
 import React, { useState } from "react";
-import { Media } from "reactstrap";
-export const Menu = () => {
-  const [dishes] = useState([
-    {
-      id: 0,
-      name: "Uthappizza",
-      image: "assets/images/uthappizza.png",
-      category: "mains",
-      label: "Hot",
-      price: "4.99",
-      description:
-        "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
-    },
-    {
-      id: 1,
-      name: "Zucchipakoda",
-      image: "assets/images/zucchipakoda.png",
-      category: "appetizer",
-      label: "",
-      price: "1.99",
-      description:
-        "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
-    },
-    {
-      id: 2,
-      name: "Vadonut",
-      image: "assets/images/vadonut.png",
-      category: "appetizer",
-      label: "New",
-      price: "1.99",
-      description:
-        "A quintessential ConFusion experience, is it a vada or is it a donut?",
-    },
-    {
-      id: 3,
-      name: "ElaiCheese Cake",
-      image: "assets/images/elaicheesecake.png",
-      category: "dessert",
-      label: "",
-      price: "2.99",
-      description:
-        "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
-    },
-  ]);
+import {
+  Card,
+  CardBody,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardTitle,
+} from "reactstrap";
+
+export const Menu = (props) => {
+  const [selectedDish, setSelectedDish] = useState({});
+  const handleDishSelect = (dish) => {
+    setSelectedDish(dish);
+  };
+  const getDishDescription = (dish) => {
+    if (selectedDish !== null) {
+      return (
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <Card>
+              <CardImg src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle heading>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      );
+    }
+    else{
+        <div></div>
+    }
+  };
   return (
     <div className="container">
       <div className="row">
-        {dishes.map((dish) => {
+        {props.dishes.map((dish) => {
           return (
-            <Media key={dish.id}  list>
-              <div className="col-12 mt-5">
-                  <Media left middle>
-                    <Media object src={dish.image} alt={dish.name} />
-                  </Media>
-                  <Media body className="ml-50">
-                    <Media heading>{dish.name}</Media>
-                    <p>{dish.description}</p>
-                </Media>
-              </div>
-            </Media>
+            <div className="col-12 col-md-5 m-1">
+              <Card key={dish.id}>
+                <CardImg src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                  <CardTitle
+                    style={{ cursor: "pointer" }}
+                    heading
+                    onClick={() => handleDishSelect(dish)}
+                  >
+                    {dish.name}
+                  </CardTitle>
+                </CardImgOverlay>
+              </Card>
+            </div>
           );
         })}
       </div>
+      {getDishDescription(selectedDish)}
     </div>
   );
 };
