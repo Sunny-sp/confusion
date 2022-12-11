75be579faf05a157a0f5199ea158ec5a258d25d0
Sunny Prajapati <prajapatisunny222@gmail.com>
2022-12-11 22:56:13 +0530

    fixed the smell code function inside render

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -23,35 +23,44 @@ const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
 });
 
+const HomePage = (props) => {
+  return (
+      <Home
+      dish={props.dishes.filter(dish => dish.featured === true)[0]}
+      promotion={props.promotions.filter(promotion => promotion.featured === true)[0]}
+      leader={props.leaders.filter(leader => leader.featured === true)[0]}
+      />
+  );
+};
+
+const DishWithId = (props) => {
+  const { id } = useParams();
+  return (
+    <DishDetail
+    dish={props.dishes.filter((dish) => dish.id === parseInt(id, 10))[0]}
+    comments={props.comments.filter((comment) => comment.dishId === parseInt(id, 10))}
+    addComment = {props.addComment}
+    />
+  );
+};
+
 class Main extends Component {
   render () {
-    const HomePage = () => {
-      return (
-          <Home
-          dish={this.props.dishes.filter(dish => dish.featured === true)[0]}
-          promotion={this.props.promotions.filter(promotion => promotion.featured === true)[0]}
-          leader={this.props.leaders.filter(leader => leader.featured === true)[0]}
-          />
-      );
-    };
-    const DishWithId = () => {
-      const { id } = useParams();
-      return (
-        <DishDetail
-        dish={this.props.dishes.filter((dish) => dish.id === parseInt(id, 10))[0]}
-        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(id, 10))}
-        addComment = {this.props.addComment}
-        />
-      );
-    };
-
     return (
       <div>
         <Header />
           <Routes>
-            <Route exact path='/home' element={<HomePage/>} />
+            <Route exact path='/home'
+              element={<HomePage
+                dishes={this.props.dishes}
+                promotions={this.props.promotions}
+                leaders={this.props.leaders}/>} />
             <Route exact path='/menu'element={<Menu dishes={this.props.dishes}/>}/>
-            <Route path='/menu/:id' element={<DishWithId/>}/>
+            <Route path='/menu/:id'
+              element={<DishWithId
+                dishes={this.props.dishes}
+                comments={this.props.comments}
+                addComment = {this.props.addComment}/>}/>
             <Route exect path='/contactus' element={<Contact/>}/>
             <Route exact path='/aboutus' element={<AboutUs leaders={this.props.leaders}/>} />
             <Route path='*' element={<Navigate to='/home'/>}/>
