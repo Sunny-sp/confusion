8b5c46f6ef9bb00e3454403f0a2852c1a2d6dfcd
Sunny Prajapati <prajapatisunny222@gmail.com>
2023-03-29 12:19:07 +0530

    updated the contact and address

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -39,9 +39,8 @@ function Contact (props) {
             <div className="col-12 col-sm-4 offset-sm-1">
                     <h5>Our Address</h5>
                     <address>
-                    121, Clear Water Bay Road<br />
-                    Clear Water Bay, Kowloon<br />
-                    HONG KONG<br />
+                    Om Shivai building wing-A2<br />
+                    Borivali East Mumbai<br />
                     <i className="fa fa-phone"></i>: +852 1234 5678<br />
                     <i className="fa fa-fax"></i>: +852 8765 4321<br />
                     <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -17,11 +17,11 @@ function Footer () {
                     <div className="col-7 col-sm-5">
                         <h5>Our Address</h5>
                         <address>
-                        121, Clear Water Bay Road<br />
-                        Clear Water Bay, Kowloon<br />
-                        HONG KONG<br />
-                        <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
-                        <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
+                        Om Shivai building wing-A2<br />
+                        Borivali East Mumbai<br />
+                        India<br />
+                        <i className="fa fa-phone fa-lg"></i>: +91 9834567856<br />
+                        <i className="fa fa-fax fa-lg"></i>: +91 8765224321<br />
                         <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
                             confusion@food.net</a>
                         </address>
